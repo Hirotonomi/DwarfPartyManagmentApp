2ed03743e452b2b33eef3dfd0938911437e9ed76
Hirotonomi <Asztonpsiak@gmail.com>
2024-03-09 13:15:31 +0100

    Changed hard coded info, to info mapped from external list of objects (preperations for api)

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,14 @@
 
 import './App.css'
 import Row from './RowModule/Row.jsx'
+import tableData from './apiShit/GettingData.jsx'
+
+const TableCon = tableData.map(row => <Row name={row.name} DNDc={row.DNDc} hp={row.hp} gold={row.gold}/>)
 
 function App() {
   return (
     <>
-      <Row name="D Domper" DNDc="Palladyn" hp={80} gold={500}/>
-      <Row name="JJanik" DNDc="Mag" hp={46} gold={170 }/>
-      <Row name="Jyn Romper" DNDc="Barbażyńca" hp={90} gold={2}/>
-      <Row name="Maten Pasnek" DNDc="Bard" hp={27} gold={420}/>
+      {TableCon}
       <Row/>
  
     </>
---
+++ src/apiShit/GettingData.jsx
@@ -0,0 +1,7 @@
+const tableData = [
+    { id: 999, name: "D Domper", DNDc: "Palladyn", hp: 80, gold: 500 },
+    { id: 1000, name: "JJanik", DNDc: "Mag", hp: 46, gold: 170 },
+    { id: 1001, name: "Jyn Romper", DNDc: "Barbażyńca", hp: 90, gold: 2 },
+    { id: 1002, name: "Maten Pasnek", DNDc: "Bard", hp: 27, gold: 420 }
+]
+export default tableData
