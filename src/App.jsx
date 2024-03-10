6853aa297ea35ac662e279ab57c1d058c28bb5b5
Hirotonomi <Asztonpsiak@gmail.com>
2024-03-10 10:17:28 +0100

    leaving the branch for now cause i porwałem się z motyką na słońce, and i still dont realy understand use effect and use state so gona wait some more and do lil more courses

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,15 @@
-
-import './App.css'
 import Row from './RowModule/Row.jsx'
 import tableData from './apiShit/GettingData.jsx'
+import DomberComponent from './domberModule/DomberComponent.jsx'
 
-const TableCon = tableData.map(row => <Row name={row.name} DNDc={row.DNDc} hp={row.hp} gold={row.gold}/>)
+const TableCon = tableData.map(row => <Row key={row.id} name={row.name} DNDc={row.DNDc} hp={row.hp} gold={row.gold}/>)
 
 function App() {
   return (
     <>
       {TableCon}
       <Row/>
+      <DomberComponent/>
  
     </>
   )
--- src/domberModule/DomberComponent.jsx
+++ src/domberModule/DomberComponent.jsx
@@ -1,6 +1,26 @@
+import React, {useState, useEffect} from 'react'
+import styles from './DomberComponent.module.css'
+
 function DomberComponent(){
+
+    var [count, setCount] = useState(0)
+
+    function changeCount(d){
+        setCount(c => d.innerHTML.length)   
+    }
+
+    useEffect(()=>{
+        var d = document.getElementById("inputTextForCount")
+        changeCount(d)
+
+    
+    })
+
     return(
-    <div>
-        <input type="text" placeholder="Enter text here..."></input>
+    <div className={styles.row}>
+        <input id="inputTextForCount"  type="text" placeholder="Just Try Me Bro"></input>
+        <p>Liczba znaków: {count}</p>
     </div>)
 }
+
+export default DomberComponent
