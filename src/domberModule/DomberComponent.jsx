854e0c3989e1c95b21fba3a9e46808c70856b441
Hirotonomi <Asztonpsiak@gmail.com>
2024-03-10 12:13:57 +0100

    setState modifications used for my education

--- src/domberModule/DomberComponent.jsx
+++ src/domberModule/DomberComponent.jsx
@@ -1,26 +1,42 @@
-import React, {useState, useEffect} from 'react'
+import React, { useState, useEffect } from 'react'
 import styles from './DomberComponent.module.css'
 
-function DomberComponent(){
+function DomberComponent() {
 
-    var [count, setCount] = useState(0)
+    const [count, setCount] = useState(0)
+    const [eCount, setECount] = useState(0)
 
-    function changeCount(d){
-        setCount(c => d.innerHTML.length)   
+    function addCount() {
+        setCount(count + 1)
     }
 
-    useEffect(()=>{
-        var d = document.getElementById("inputTextForCount")
-        changeCount(d)
+    function minusCount() {
+        setCount(count - 1)
+    }
 
-    
-    })
+    function resetCount() {
+        setCount(document.getElementById("inputTextForCount").value.length)
+    }
 
-    return(
-    <div className={styles.row}>
-        <input id="inputTextForCount"  type="text" placeholder="Just Try Me Bro"></input>
-        <p>Liczba znaków: {count}</p>
-    </div>)
+    return (
+        <>
+            <div className={styles.row}>
+                <input id="inputTextForCount" type="text" placeholder="Type in me Daddy"></input>
+                <p>Liczba znaków: {count}</p>
+            </div>
+
+            <div className={styles.buttonThingy}>
+                <p>{count}</p>
+                <button onClick={minusCount}>minus</button>
+                <button onClick={resetCount}>reset</button>
+                <button onClick={addCount}>plus</button>
+            </div>
+
+            <div className={styles.row}>
+                <input id="inputTextForEffect" type="text" placeholder="Just Try Me Bro"></input>
+                <p>Liczba znaków: {eCount}</p>
+            </div>
+        </>)
 }
 
 export default DomberComponent
