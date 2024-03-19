b11a35a5136ffa1f88c015a49defbb1037885fa5
Hirotonomi <Asztonpsiak@gmail.com>
2024-03-20 00:41:07 +0100

    FILTER RAAAH DWIE TABELE STACKOVERFLOW I GPT I DZIAŁA

--- src/TableModule/Table.jsx
+++ src/TableModule/Table.jsx
@@ -1,12 +1,22 @@
+import { useState } from 'react'
 import Row from '../RowModule/Row.jsx'
 import tableData from '../apiShit/GettingData.jsx'
-
-const TableCon = tableData.map(row => <Row key={row.id} name={row.name} DNDc={row.DNDc} hp={row.hp} gold={row.gold}/>)
+import styles from './Table.module.css'
 
 function Table() {
+    const [tableDataS, setTableDataS] = useState(tableData) 
+    const [filtredTable, setFiltredTable] = useState(tableDataS)
+
+    function searchChange(e){
+        var input = e.target.value
+        setFiltredTable(tableDataS.filter(row => row.name.toLowerCase().includes(input.toLowerCase())))
+    }
+    
   return (
     <>
-      {TableCon} 
+    <div className={styles.inpt}><span>Filtruj po imieniu</span><input onChange={searchChange} type='text' ></input></div>
+
+    {filtredTable.map(row => <Row key={row.id} name={row.name} DNDc={row.DNDc} hp={row.hp} gold={row.gold}/>)} 
     </>
   )
 }
